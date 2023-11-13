27e575169f7c91de4c05c0f477c302f0c95b16ee
David Sullivan <treatkor@gmail.com>
2023-11-13 12:39:43 -0800

    handle existing file names

--- src/preload/convert.js
+++ src/preload/convert.js
@@ -1,61 +1,46 @@
 import fs from 'node:fs'
 import sharp from 'sharp'
-const convert = async (file, format, out_directory, append_string, options) => {
-  let basePath = process.argv.slice(-1)[0]
-  const buffer = Buffer.from(await file.arrayBuffer())
-  // console.log('---OPTIONS---')
-  // console.log(options)
+import path from 'path'
 
-  // options = coerceTypes(options)
-  // console.log('---correctedtypes options---')
-  // console.log(options)
-  // console.log('---options---')
-  let filepath
-  let re = /\.[^.]*$/gm
-  let newname = file.name.replace(re, append_string)
-  let filename = `/${newname}.${format}`
+const re = /\.[^.]*$/gm
 
-  if (!out_directory) {
-    fs.mkdir(basePath, { recursive: true }, (err) => {
-      if (err) throw err
-    })
-    filepath = `${basePath}${filename}`
-  } else {
-    fs.mkdir(out_directory, { recursive: true }, (err) => {
-      if (err) throw err
-    })
-    filepath = `${out_directory}${filename}`
+function checkFileExistenceAndIncrementFilename(filepath) {
+  // Check if file exists and increment the filename if it does.
+  const dirname = path.dirname(filepath)
+  const extname = path.extname(filepath)
+  const filenameBase = path.basename(filepath, extname)
+  let filenameNumber = 0
+  while (fs.existsSync(filepath)) {
+    filenameNumber++
+    filepath = path.join(dirname, `${filenameBase}_${filenameNumber}${extname}`)
   }
+  return filepath
+}
+
+const convert = async (file, format, out_directory, append_string, options) => {
+  let buffer = Buffer.from(await file.arrayBuffer())
+  let filepath
+  let filename = file.name.replace(re, append_string) + '.' + format
+  if (!out_directory) {
+    out_directory = process.argv.slice(-1)[0]
+  }
+  fs.mkdir(out_directory, { recursive: true }, (err) => {
+    if (err) throw err
+  })
+  // filepath = path.join(out_directory, filename)
+  filepath = checkFileExistenceAndIncrementFilename(path.join(out_directory, filename))
 
   return sharp(buffer)
     .toFormat(format, options)
     .toBuffer()
     .then((data) => {
       fs.writeFileSync(filepath, data, 'base64')
+      filename = path.basename(filepath)
       return { filename, filepath, imageFormat: format }
     })
     .catch((err) => {
       console.log(`error: ${err}`)
     })
 }
-
-// const coerceTypes = (options) => {
-//   Object.entries(options).forEach((e) => {
-//     options[e[0]] = coerceValue(e[1])
-//   })
-//   return options
-// }
-
-// const coerceValue = (value) => {
-//   if (value == 'true') {
-//     return true
-//   } else if (value == 'false') {
-//     return false
-//   } else if (!isNaN(parseFloat(value))) {
-//     return parseFloat(value)
-//   } else {
-//     return value
-//   }
-// }
 
 export default convert
