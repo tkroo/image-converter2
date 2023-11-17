78d0c60b331af26ba8a11a11a8252581c1ed9c3b
David Sullivan <treatkor@gmail.com>
2023-11-17 15:31:55 -0800

    build minify true

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -3,12 +3,21 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      minify: true
+    }
   },
   preload: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      minify: true
+    }
   },
   renderer: {
-    plugins: [svelte()]
+    plugins: [svelte()],
+    build: {
+      minify: true
+    }
   }
 })
