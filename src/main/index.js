dc15cbff5ce5dbb89477fb0715d255cdd3ca0824
David Sullivan <treatkor@gmail.com>
2023-12-11 12:04:12 -0800

    separate render index for development

--- src/main/index.js
+++ src/main/index.js
@@ -72,7 +72,7 @@ function createWindow() {
   // HMR for renderer base on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+    mainWindow.loadURL(`${process.env['ELECTRON_RENDERER_URL']}/dev.html`)
   } else {
     mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
   }
